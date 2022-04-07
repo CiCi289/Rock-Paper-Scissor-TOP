98f432cf1705f434a74ab013ab23e26148aaf14c
CiCi289 <cihtinaung289.cha@gmail.com>
2022-04-07 21:19:02 +0630

    WIP Code as TOP instruction

--- Rock Paper Scissor JS.js
+++ Rock Paper Scissor JS.js
@@ -1,10 +1,26 @@
 
-
-let computerChoices =   [{choice: 'Rock', value: 0},
-                        {choice: 'Paper', value: 1},
-                        {choice: 'Scissor', value: 2}];
+//input from Player , input from Computer
+let playerChoices;
+let computerChoices = ['rock', 'paper', 'scissors'];
+let playerScore
 
 function computerPlay () {
     let result = computerChoices[Math.floor(Math.random() * computerChoices.length)];
     return result;
+};
+
+//function that plays a round wtih two parameters playerSelection and computerSelection
+function playRound (playerSelection, computerSelection) {
+    let playerPoint = 0;
+
+    if  ((playerSelection == 'rock' && computerSelection == 'scissors') ||
+        (playerSelection == 'scissors' && computerSelection == 'paper') ||
+        (playerSelection == 'paper' && computerSelection == 'rock')) {
+            playerPoint = 1;
+            console.log ('You win!' + playerSelection + ' beats ' + computerSelection)
+        } else {
+            console.log('You lose!' + computerSelection + ' beats ' + playerSelection)
+        } 
+    return playerPoint
+
 }
