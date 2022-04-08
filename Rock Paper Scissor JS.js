98852fcf427f7ea81d066ae02dcc5acaf0029b2f
CiCi289 <cihtinaung289.cha@gmail.com>
2022-04-08 18:17:19 +0630

    Lesson Learnt: Mistake in calling function which should also pass Parameters

--- Rock Paper Scissor JS.js
+++ Rock Paper Scissor JS.js
@@ -1,26 +1,54 @@
 
-//input from Player , input from Computer
-let playerChoices;
-let computerChoices = ['rock', 'paper', 'scissors'];
-let playerScore
-
+//function computerPlay() that will randomly return either Rock, Paper or Scissors
 function computerPlay () {
-    let result = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    let options = ['rock', 'paper', 'scissors'];
+    let result = options[Math.floor(Math.random() * options.length)];
     return result;
 };
 
-//function that plays a round wtih two parameters playerSelection and computerSelection
+//extra: function playerPlay() for player input
+function playerPlay() {
+    let input = prompt('Please enter input','');
+    let caseInsenInput = input.toLowerCase();
+    return caseInsenInput;
+};
+
+//function playRound() that plays a round wtih two parameters playerSelection and computerSelection
 function playRound (playerSelection, computerSelection) {
-    let playerPoint = 0;
+        
+    if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
+    (playerSelection == 'paper' && computerSelection == 'rock') ||
+    (playerSelection == 'scissors' && computerSelection == 'paper')) {
+        console.log (`Player wins!`);
 
-    if  ((playerSelection == 'rock' && computerSelection == 'scissors') ||
-        (playerSelection == 'scissors' && computerSelection == 'paper') ||
-        (playerSelection == 'paper' && computerSelection == 'rock')) {
-            playerPoint = 1;
-            console.log ('You win!' + playerSelection + ' beats ' + computerSelection)
-        } else {
-            console.log('You lose!' + computerSelection + ' beats ' + playerSelection)
-        } 
-    return playerPoint
+    // } else if (typeof(playerSelection) === 'undefined' && typeof(computerSelection) === "undefined") {
+    //     console.log('parameters are undefined');
+    
+    
+    } else if (playerSelection === computerSelection) {
+        console.log (`Draw!`);
 
-}
+    } else {
+        console.log (`Computer wins!`);
+    }
+
+};
+//function game() that calls playRound() inside to play 5 rounds of game,
+//keep scores
+//declare winner or loser at the end.
+function game() {
+
+    for(let i = 1; i < 6; i++) {
+
+        let playerSelection = playerPlay();
+        let computerSelection = computerPlay();
+
+        console.log(`[Play turn ${i}] Player chose: ${playerSelection} | Computer chose: ${computerSelection}`);
+        console.log(playRound(playerSelection, computerSelection)); // <===* i got hugeee problem here and couldnt find where to fix for so long when i just used playRound() only.
+        //Here is what went wrong...
+        //I am not passing parameters to playerSelection and computerSelection that the constructor of playRound() expects. playRound() gets no paramenter string values.
+        //So, both variables are set to "undefined" and you have no condition in your code to handle that. 
+        //Alternatively, remove it from the constructor if you want to use the globally declared variables of the same name.
+
+    }
+};
