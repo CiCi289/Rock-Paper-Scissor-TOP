39fc1ac697357bcfbc277bb591431b4f50fd57f1
CiCi289 <cihtinaung289.cha@gmail.com>
2022-04-17 14:15:00 +0630

    RPS before moving to DOM

--- Rock Paper Scissor JS.js
+++ Rock Paper Scissor JS.js
@@ -1,3 +1,5 @@
+let playerScore = 0;
+let computerScore = 0;
 
 //function computerPlay() that will randomly return either Rock, Paper or Scissors
 function computerPlay () {
@@ -15,24 +17,36 @@ function playerPlay() {
 
 //function playRound() that plays a round wtih two parameters playerSelection and computerSelection
 function playRound (playerSelection, computerSelection) {
+    let WinnerResult = '';
         
     if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
     (playerSelection == 'paper' && computerSelection == 'rock') ||
     (playerSelection == 'scissors' && computerSelection == 'paper')) {
-        console.log (`Player wins!`);
+        console.log (`Player wins the round!`);
+        playerScore += 1;
 
     // } else if (typeof(playerSelection) === 'undefined' && typeof(computerSelection) === "undefined") {
-    //     console.log('parameters are undefined');
-    
+    //     console.log('parameters are undefined'); 
+    //not passing parameter to playRound() problem log test
     
     } else if (playerSelection === computerSelection) {
-        console.log (`Draw!`);
-
+        console.log (`Round Draw!`);
+        
     } else {
-        console.log (`Computer wins!`);
-    }
+        console.log (`Computer wins the round!`);
+        computerScore += 1;
+    };
 
+    if (playerScore > computerScore) {
+        WinnerResult = (`Player has won the game!`);
+    } else if (computerScore > playerScore) {
+        WinnerResult = (`Computer has won the game!`);
+    } else {
+        WinnerResult = (`No winners! It's a Draw!`);
+    }
+    console.log(WinnerResult);
 };
+
 //function game() that calls playRound() inside to play 5 rounds of game,
 //keep scores
 //declare winner or loser at the end.
@@ -45,10 +59,10 @@ function game() {
 
         console.log(`[Play turn ${i}] Player chose: ${playerSelection} | Computer chose: ${computerSelection}`);
         console.log(playRound(playerSelection, computerSelection)); // <===* i got hugeee problem here and couldnt find where to fix for so long when i just used playRound() only.
-        //Here is what went wrong...
-        //I am not passing parameters to playerSelection and computerSelection that the constructor of playRound() expects. playRound() gets no paramenter string values.
-        //So, both variables are set to "undefined" and you have no condition in your code to handle that. 
-        //Alternatively, remove it from the constructor if you want to use the globally declared variables of the same name.
+//Here is what went wrong...
+//I am not passing parameters to playerSelection and computerSelection that the constructor of playRound() expects. playRound() gets no paramenter string values.
+//So, both variables are set to "undefined" and you have no condition in your code to handle that. 
+//Alternatively, remove it from the constructor if you want to use the globally declared variables of the same name.
 
     }
 };
